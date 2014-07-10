1d658b4c72046efd722d5b23e451924adb5a8b29
Simon Hofmann <mail@simon-hofmann.org>
2014-07-10 12:20:20 +0200

    Continued work on IPC class

--- ipc.py
+++ ipc.py
@@ -1,32 +1,31 @@
 #!/usr/bin/env python
+""" This module provides a class for inter process communication
+     with the Spotify desktop application. """
 # encoding: utf-8
 
 import dbus
 
 class IPCHandler(object):
 
-    """Handler class for IPC communication with the Spotify application"""
+    """Handler class for inter process communication
+     with the Spotify desktop application"""
 
     def __init__(self):
         """Initialize IPC communication with Spotify application """
 
         try:
-            self.bus = dbus.SessionBus()
+            self.__bus = dbus.SessionBus()
 
-            self.main_obj = self.bus.get_object('com.spotify.qt', '/')
-            self.props_obj = self.bus.get_object('com.spotify.qt',
-                                                 '/org/mpris/MediaPlayer2')
+            self.__player_obj = self.__bus.get_object('com.spotify.qt', '/')
+            self.__props_obj = self.__bus.get_object('com.spotify.qt',
+                                                     '/org/mpris/MediaPlayer2')
 
-            self.main_interface = dbus.Interface(self.main_obj,
-                                                 'org.\
-                                                  freedesktop.\
-                                                  MediaPlayer2')
+            self.__player_interface = dbus.Interface(self.__player_obj,
+                                                     'org.freedesktop.MediaPlayer2')
 
-            self.props_interface = dbus.Interface(self.props_obj,
-                                                  'org.\
-                                                   mpris.\
-                                                   MediaPlayer2.\
-                                                   Player')
+            self.__props_interface = dbus.Interface(self.__props_obj,
+                                                    'org.freedesktop.DBus.Properties')
+
         except dbus.exceptions.DBusException:
             raise
 
@@ -35,71 +34,170 @@ class IPCHandler(object):
         :returns: 1 if reconnect was successfull, 0 if otherwise
 
         """
+        #TODO: Implement reconnect method
         pass
 
     def __read_property(self, prop):
         """Returns the value of the given proptery
 
-        :prop: Desired property value
-        :returns: Value
+        :prop: String
+        :returns: value
 
         """
-        pass
+
+        try:
+            ret_val = self.__props_interface.Get('org.mpris.MediaPlayer2.Player',
+                                                 '%s' % prop)
+            return ret_val
+
+        except dbus.exceptions.DBusException:
+            raise
+
+    def __set_property(self, prop, val):
+        """Sets a property to the given value
+
+        :prop: string
+        :val: value
+        :returns: none
+
+        """
+        try:
+            self.__props_interface.Set('org.mpris.MediaPlayer2.Player',
+                                       '%s' % prop,
+                                       '%s' % val)
+
+        except dbus.exceptions.DBusException:
+            raise
 
     def get_shuffle_mode(self):
         """Check if shuffle mode is enabled or disabled.
         :returns: True if shuffle is enabled, false if not
 
         """
-        pass
+        return self.__read_property('Shuffle')
 
-    def set_shuffle_mode(self, state):
-        """Enable or disable shuffle mode
+    def toggle_shuffle(self):
+        """Toggle shuffle mode
 
-        :bool state: True = enabled, false = disabled
         :returns: none
 
         """
-        pass
+        try:
+            if self.__read_property('Shuffle'):
+                self.__set_property('Shuffle', False)
+            else:
+                self.__set_property('Shuffle', True)
+
+        except dbus.exceptions.DBusException:
+            raise
 
     def get_playback_status(self):
         """Get the current playback status
         :returns: 1 if playing, 0 if paused
 
         """
-        pass
+        try:
+            return self.__props_interface.Get('org.mpris.MediaPlayer2.Player',
+                                              'PlaybackStatus')
+
+        except dbus.exceptions.DBusException:
+            raise
 
     def get_artist(self):
         """Get artist of current played track
-        :returns: string with artist name
+        :returns: string
 
         """
-        pass
+        try:
+            artist = self.__read_property('Metadata')
+
+            return artist['xesam:artist'][0]
+        except dbus.exceptions.DBusException:
+            raise
 
     def get_title(self):
         """Get title of current track
-        :returns: string with track title
+        :returns: string
 
         """
-        pass
+        try:
+            title = self.__read_property('Metadata')
+
+            return title['xesam:title']
+        except dbus.exceptions.DBusException:
+            raise
 
     def get_album(self):
         """Get album name of current track
-        :returns: string with album title
+        :returns: string
 
         """
-        pass
+        try:
+            album = self.__read_property('Metadata')
+
+            return album['xesam:album']
+        except dbus.exceptions.DBusException:
+            raise
 
     def play_next(self):
         """Skip to the next song if possible
         :returns: none
 
         """
-        pass
+        try:
+            if self.__read_property('CanGoNext'):
+                self.__player_interface.Next()
+        except dbus.exceptions.DBusException:
+            raise
+
 
     def play_previous(self):
         """Skip to the previous song if possible
         :returns: none
 
         """
+        try:
+            if self.__read_property('CanGoPrevious'):
+                self.__player_interface.Previous()
+        except dbus.exceptions.DBusException:
+            raise
+
+    def play_pause(self):
+        """Toggle between play/pause mode
+        :returns: none
+
+        """
+        try:
+            self.__player_interface.PlayPause()
+        except dbus.exceptions.DBusException:
+            raise
+
+    def get_volume(self):
+        """Returns current volume value
+        :returns: double
+
+        """
         pass
+
+    def set_volume(self, vol):
+        """Set volume to given value
+
+        :vol: double
+        :returns: none
+
+        """
+        pass
+
+    def get_position(self):
+        """Returns the position in current track
+        :returns: int
+
+        """
+        pass
+
+# For testing purposes only
+if __name__ == '__main__':
+    test = IPCHandler()
+    print(test.get_album())
+    test.play_pause()
+    test.play_next()
